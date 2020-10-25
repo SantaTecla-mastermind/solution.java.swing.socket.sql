4d1b234cabe7d5dee0ecfa204a1ab3dfae91cc64
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2020-10-25 18:55:17 +0100

    ProposedCombination y SecretCombination actualizadas

--- documentView/withFactoryMethod/src/main/java/usantatecla/mastermind/models/ProposedCombination.java
+++ documentView/withFactoryMethod/src/main/java/usantatecla/mastermind/models/ProposedCombination.java
@@ -9,12 +9,7 @@ public class ProposedCombination extends Combination {
 	}
 
 	boolean contains(Color color) {
-		for (int i = 0; i < this.colors.size(); i++) {
-			if (this.colors.get(i) == color) {
-				return true;
-			}
-		}
-		return false;
+		return this.colors.contains(color);
 	}
 
 	public List<Color> getColors() {
