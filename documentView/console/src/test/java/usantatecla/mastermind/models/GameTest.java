65396a8cfdaba92218b7a26562302c7c694b947c
MRxRafi <rafadriveclase@gmail.com>
2020-11-07 23:38:45 +0100

    [dv.console] GameTest implemented with GivenWhenThen naming and assertThat assertions

---
+++ documentView/console/src/test/java/usantatecla/mastermind/models/GameTest.java
@@ -0,0 +1,100 @@
+package usantatecla.mastermind.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void before() {
+        this.game = new Game();
+    }
+
+    @Test
+    void testGivenEmptyGameInstanceWhenIsLooserOrWinnerThenIsFalse() {
+        assertThat(this.game.isLooser(), is(equalTo(false)));
+        // TODO isWinner IndexOutOfBounds preguntar reunion 12/11
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> this.game.isWinner());
+    }
+
+    @Test
+    void testGivenEmptyGameInstanceWhenGetAttemptsThenIs0() {
+        assertThat(this.game.getAttempts(), is(equalTo(0)));
+    }
+
+
+    @Test
+    void testGivenNineAttemptsGameWhenPlaceAnotherProposedCombinationThenIsLooser() {
+        ArrayList<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW));
+        ProposedCombination combination = new ProposedCombination();
+        combination.colors = colors;
+        for(int i = 0; i < 9; i++) {
+            this.game.addProposedCombination(combination);
+        }
+        assertThat(this.game.isLooser(), is(false));
+
+        this.game.addProposedCombination(combination);
+        assertThat(this.game.isLooser(), is(true));
+    }
+
+    @Test
+    void testGivenGameWith1ProposedCombinationWhenGetProposedCombinationAtPosition0ThenReturnsThatProposedCombination() {
+        ArrayList<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW));
+        ProposedCombination combination = new ProposedCombination();
+        combination.colors = colors;
+        this.game.addProposedCombination(combination);
+
+        assertThat(this.game.getProposedCombination(0), is(equalTo(combination)));
+    }
+
+    @Test
+    void testGivenGameWith3ProposedCombinationsWhenGetAttemptsThenReturns3() {
+        ArrayList<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW));
+        ProposedCombination combination = new ProposedCombination();
+        combination.colors = colors;
+        this.game.addProposedCombination(combination);
+        this.game.addProposedCombination(combination);
+        this.game.addProposedCombination(combination);
+
+        assertThat(this.game.getAttempts(), is(3));
+    }
+
+    @Test
+    void testGivenGameWith1ProposedCombinationWhenGetResultAt0ThenReturnsValidResult() {
+        ArrayList<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW));
+        ProposedCombination combination = new ProposedCombination();
+        combination.colors = colors;
+        this.game.addProposedCombination(combination);
+
+        assertThat(this.game.getResult(0), notNullValue(Result.class));
+    }
+
+    @Test
+    void testGivenGameInAnyStateWhenResetThenIsEmpty() {
+        ArrayList<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW));
+        ProposedCombination combination = new ProposedCombination();
+        combination.colors = colors;
+        Random random = new Random(System.currentTimeMillis());
+        for(int i = 0; i < random.nextInt(10); i++) {
+            this.game.addProposedCombination(combination);
+        }
+
+        this.game.reset();
+
+        assertThat(this.game.getAttempts(), is(0));
+        assertThat(this.game.isLooser(), is(false));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> this.game.isWinner());
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> this.game.getProposedCombination(0));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> this.game.getResult(0));
+    }
+}
