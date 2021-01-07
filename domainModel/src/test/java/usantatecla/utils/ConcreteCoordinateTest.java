64b24febcd6650aa1745d505c1d95a1ebc9c3df3
Pablo <pabloguijarro96@gmail.com>
2021-01-07 14:06:18 +0100

    New tests in ConcreteCoordinate

--- domainModel/src/test/java/usantatecla/utils/ConcreteCoordinateTest.java
+++ domainModel/src/test/java/usantatecla/utils/ConcreteCoordinateTest.java
@@ -1,11 +1,11 @@
 package usantatecla.utils;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 
 public class ConcreteCoordinateTest {
 
@@ -28,6 +28,11 @@ public class ConcreteCoordinateTest {
     }*/
 
     @Test
+    public void testGivenConcreteCoordinateWhenGetRowAndGetColumn() {
+        assertThat(this.coordinate, is(new ConcreteCoordinate(this.coordinate.getRow(), this.coordinate.getColumn())));
+    }
+
+    @Test
     public void testGivenNewConcreteCoordinateWhenToString() {
         assertThat(this.coordinate.toString(), is("Coordinate (0, 0)"));
     }
@@ -35,6 +40,27 @@ public class ConcreteCoordinateTest {
     @Test
     public void testGivenNewConcreteCoordinateWhenIsNull() {
         assertThat(this.coordinate.isNull(), is(false));
+    }
+
+    @Test
+    public void testGivenTwoConcreteCoordinatesWhenAreInHorizontal() {
+        ConcreteCoordinate coordinate1 = new ConcreteCoordinate(0,1);
+        assertThat(this.coordinate.inHorizontal(coordinate1), is(true));
+        assertThat(this.coordinate.getDirection(coordinate1), is(Direction.HORIZONTAL));
+    }
+
+    @Test
+    public void testGivenTwoConcreteCoordinatesWhenAreInVertical() {
+        ConcreteCoordinate coordinate1 = new ConcreteCoordinate(1,0);
+        assertThat(this.coordinate.inVertical(coordinate1), is(true));
+        assertThat(this.coordinate.getDirection(coordinate1), is(Direction.VERTICAL));
+    }
+
+    @Test
+    public void testGivenNewConcreteCoordinateWhenIsInMainDiagonal() {
+        ConcreteCoordinate coordinate1 = new ConcreteCoordinate(1,1);
+        assertThat(this.coordinate.inMainDiagonal(), is(true));
+        assertThat(this.coordinate.getDirection(coordinate1), is(Direction.MAIN_DIAGONAL));
     }
 
 }
