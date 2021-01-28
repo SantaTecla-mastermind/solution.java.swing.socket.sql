f76b8d684275454508a3830ece5f4875c560a224
Pablo <pabloguijarro96@gmail.com>
2021-01-28 16:07:57 +0100

    [MVP.pm.basic] Problema uso en BoardView de los controladores solucionado

--- modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/mastermind/views/console/StartView.java
+++ modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/mastermind/views/console/StartView.java
@@ -5,20 +5,19 @@ import usantatecla.mastermind.views.Message;
 
 public class StartView  {
 
-    //TODO Preguntar el startController a Luis
     private StartController startController;
     private ProposedCombinationView proposedCombinationView;
-    //private BoardView boardView;
+    private BoardView boardView;
 
     StartView(StartController startController) {
         this.startController = startController;
         this.proposedCombinationView = new ProposedCombinationView();
-       // this.boardView = new BoardView(startController, this.proposedCombinationView);
+        this.boardView = new BoardView(startController, this.proposedCombinationView);
     }
 
     void interact() {
         Message.TITLE.writeln();
-        //this.boardView.write();
+        this.boardView.write();
     }
 
 }
